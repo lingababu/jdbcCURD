1149fb982a4e0076023c46344644f9cce52a2a13
lingababu <82533772+lingababu@users.noreply.github.com>
2025-07-03 18:07:01 +0530

    Update ServiceCaller.java
    saving as e6

--- src/main/java/com/employee/caller/ServiceCaller.java
+++ src/main/java/com/employee/caller/ServiceCaller.java
@@ -21,7 +21,7 @@ public class ServiceCaller implements ApplicationRunner {
 
 		Employee employee = new Employee(1006, "John", 2500, 43);
 		
-		Employee employee3 = new Employee(1006, "John", 2500, 43);
+		Employee e6 = new Employee(1006, "John", 2500, 43);
 
 		employeeService.saveEmployee(employee);
 
